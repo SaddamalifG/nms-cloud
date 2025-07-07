9a4bdd7f8c4da5c87533ed4b07fcb193b302ea45
Saddam Alif <saddamalif3@gmail.com>
2025-07-07 19:49:52 +0700

    fix: use process.env.PORT for Railway

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
 
 app.use(cors());
 app.use(express.json());
